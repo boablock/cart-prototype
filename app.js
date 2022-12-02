3e444a02b6092cb3553c1c9625eee219327d56d7
boablock <boablockchain@gmail.com>
2022-12-01 21:36:09 -0300

    cart-v1

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 
 const contenedorProductos = document.getElementById('contenedor-productos'); 
-const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const botonVaciar = document.getElementById('vaciar-carrito');
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+
+const precioTotal = document.getElementById('precioTotal');
+
+const cantidad = document.getElementById('cantidad')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
 let carrito =[];
 
+//localStorage - getItem
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0;
+    actualizarCarrito();
+
+    console.log(carrito);
+})
+
+//PRIMER PASO, INYECTAR AL HTML
 stockProductos.forEach((producto) => {
     const div = document.createElement('div');
     div.classList.add('producto')
@@ -19,7 +45,6 @@ stockProductos.forEach((producto) => {
     const boton = document.getElementById(`agregar${producto.id}`)
 
     boton.addEventListener('click', () => {
-
     agregarAlCarrito(producto.id)
     })
 
@@ -27,14 +52,42 @@ stockProductos.forEach((producto) => {
 
 
 const agregarAlCarrito = (prodId) => {
-    const item = stockProductos.find((prod) => prod.id === prodId)
-    carrito.push(item);
+    const existe = carrito.some (prod => prod.id === prodId)
+
+    if (existe){
+        const prod = carrito.map(prod =>{
+            if(prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else {
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item);
+        console.log(carrito);
+    }
+
     actualizarCarrito();    
+}
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod)=> prod.id === prodId) // --> ya tengo el producto, ahora quiero eliminarlo del array
+
+    const indice = carrito.indexOf(item);
+
+    carrito.splice(indice, 1);
+
+    actualizarCarrito(); //--> actualizar carrito tiene un boton con evento onclick='eliminarDelCarrtio(prod.id)
+
     console.log(carrito);
 }
 
 const actualizarCarrito = () => {
+    //LOS APPENDS SE VAN ACUMULANDO CON LO QE HABIA ANTES
+    contenedorCarrito.innerHTML = "" //Cada vez que yo llame a actualizarCarrito, lo primero q hago
+    //es borrar el nodo. Y despues recorro el array lo actualizo de nuevo y lo rellena con la info
+    //actualizado
 
+    //Por cada producto creamos un div con esta estructura y le hacemos un append al contenedorCarrito (el modal)
     carrito.forEach((prod) => {
         const div = document.createElement('div');
         div.className = ('productoEnCarrito')
@@ -45,7 +98,13 @@ const actualizarCarrito = () => {
         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
         `
         contenedorCarrito.appendChild(div)
+        //localStorage - setItem
+        localStorage.setItem('carrito', JSON.stringify(carrito))
     })
+    // numero del contador carrito (cantidad de items)      
+    contadorCarrito.innerText = carrito.length; 
+    // precio total
+    precioTotal.innerText = carrito.reduce((acc,prod)=> acc + prod.precio, 0)
 }
 
 
--- stock.js
+++ stock.js
@@ -1,7 +1,7 @@
 let stockProductos = [
     {id: 1, nombre: "Benetton", cantidad: 1, precio: 2300,  img: './media/suscri8.jpg'},
-    {id: 2, nombre: "Benetton", cantidad: 1, precio: 4500,  img: './media/ramo1.webp'},
+    {id: 2, nombre: "Rosas", cantidad: 1, precio: 4500,  img: './media/ramo1.webp'},
     {id: 3, nombre: "Benetton", cantidad: 1, precio: 3500,  img: './media/suscri8.jpg'},
-    {id: 4, nombre: "Benetton", cantidad: 1, precio: 5500,  img: './media/ramo1.webp'},
+    {id: 4, nombre: "Rosas", cantidad: 1, precio: 5500,  img: './media/ramo1.webp'},
 
 ];
